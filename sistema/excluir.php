792768c0e09ddaa3e846952cae655f591ec4e0fa
paolap2006-arch <paola.p2006@aluno.ifsc.edu.br>
2025-09-23 17:51:42 -0300

    aluno matriculado

--- sistema/excluir.php
+++ sistema/excluir.php
@@ -1,7 +1,12 @@
 <?php
     include 'conexao.php';
     $id = $_POST['btnExcluir'];
+    
+    //APAGAR MATRICULA SE EXISTIR
+    $sql = $pdo -> prepare("SELECT * FROM matricula WHERE aluno = ?");
+    $sql -> execute([$id]); 
 
+    //APAGAR O ALUNO
     $sql = $pdo->prepare("DELETE FROM Aluno WHERE id = ?");
     $sql->execute([$id]);
 
